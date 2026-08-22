340fc19239d3ab7afb05b2fe7af1c78f45991344
Shoaib <shoaibchemistrysust@gmail.com>
2026-08-22 11:08:06 +0600

    setter and getter added

---
+++ src/getter-setter.ts
@@ -0,0 +1,33 @@
+
+class User {
+    _name: string
+    _age: number
+
+    constructor(givenName: string, givenAge: number ) {
+        this._name = givenName
+        this._age = givenAge
+    }
+
+    //getter don't need argument. It is used to get value
+    get age() {
+        return this._age;
+    }
+
+    set age(givenAge: number) {
+        if(givenAge < 0 || givenAge > 120) {
+            throw new Error('Age is not valid !')
+        }
+
+        this._age = givenAge;
+    }
+
+
+}
+
+
+
+const user1 = new User('Kalia', 12);
+//setter: for small validation . Method is not needed for small action. One argument can be given in setter. setter don't return anything
+
+user1.age = 40;
+console.log(user1.age);
